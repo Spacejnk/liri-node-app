e9aa1137bf604d67bcbbfd60d15af0f65b390573
Timits1 <will4f21@yahoo.com>
2019-08-20 04:46:22 -0700

    added switch statement and liribot

---
+++ app.js
@@ -0,0 +1,3 @@
+var  SPOTIFY_ID = "2d8887942dd7463e8315102df1bc9651";
+var SPOTIFY_SECRET = " b96cad0de3984e0a95be833a1cce376b";
+console.log('hello')
--- liri.js
+++ liri.js
@@ -1,104 +1,164 @@
 // At the top of the liri.js file, add code to read and set any environment variables with the dotenv package:
 require("dotenv").config();
-
-// Add the code required to import the keys.js file and store it in a variable.
+var axios = require("axios");
 var keys = require("./keys.js");
+var moment = require("moment");
+var fs = require('fs');
+var readMe = fs.readFileSync('random.txt', 'utf8');
 
-
-//You should then be able to access your keys information like so
-//
+var userInputChoice = process.argv[2];
 
 // Not working yet -- from ReadMe instructions Aug 14th 2019
 //var spotify = new Spotify(keys.spotify);
 
-// GET request for remote image
-
-//------------------------------------ 
-
-var fs = require('fs');
-var readMe = fs.readFileSync('random.txt', 'utf8');
-//console.log(readMe);
-
-//------------------------------------ 
-
-// // Must require
-// const axios = require('axios');
-// const url = 'https://jsonplaceholder.typicode.com/users';
- 
-// // Make a request for a user with a given ID
-// axios.get(url)
-//   .then(function (response) {
-//     // handle success
-//     console.log(response.data[0].address);
-//     console.log(response.data[1].name);
-//   })
-//   .catch(function (error) {
-//     // handle error
-//     console.log(error);
-//   })
-//   .finally(function () {
-//     // always executed
-//   });
-//var liriInfoChoice = process.argv[3];
-
-var artistChoice = process.argv[2];
-
-var Spotify = require('node-spotify-api');
-
+// -------------------------------------------------- spotify.search
+ var Spotify = require('node-spotify-api');
+//spotifyThisSong(userInputChoice);
+// spotify-this-song function
+function spotifyThisSong(userInputChoice) {
 var spotify = new Spotify({
-  id: '2d8887942dd7463e8315102df1bc9651',
-  secret: 'b96cad0de3984e0a95be833a1cce376b'
+id:  "2d8887942dd7463e8315102df1bc9651",
+secret:  "b96cad0de3984e0a95be833a1cce376b"
 });
 
+// Spotify Error Needs Work
+spotify.search({ type: 'artist', query: userInputChoice, limit: 5}, function(err, data) {
 
-spotify.search({ type: 'artist', query: artistChoice, limit: 5}, function(err, data) {
+
   if (err) {
     return console.log('Error occurred: ' + err);
+    
   } 
   
-
   const results = data.artists;
   const artists = results.items;
 
-  
-
   for (let i = 0 ; i < artists.length ; i++) {
-    console.log(artists[i].name
-      
-    );
-  }
+    console.log(artists[i].name);
+  };
 
-
-
+  
 });
 
+};
 
-
-
-// Basic Node application for requesting data from the OMDB website via axios
-// Here we incorporate the "axios" npm package
-var axios = require("axios");
-//var movieName = "";
-var movieThis = process.argv[2];
-
-
-// We then run the request with axios module on a URL with a JSON
-axios.get("http://www.omdbapi.com/?t=" + movieThis + "&y=&plot=short&apikey=trilogy").then(
+// --------------------------------------------------  
+var mrNobody = "If you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/";
+var netflix = "It's on Netflix!";
+// Function movieThis
+//movieThis(userInputChoice);
+function movieThis(userInputChoice){
+//var userInputChoice = process.argv[2];
+axios.get("http://www.omdbapi.com/?t=" + userInputChoice + "&y=&plot=short&apikey=trilogy").then(
   function(response) {
-    // Then we print out the imdbRating
-    console.log("Title of the movie: " + response.data.Title);
-    console.log("---------------Data---------------");
-    console.log("Year the movie came out: " + response.data.Year);
-    console.log("---------------Data---------------");
-    console.log("IMDB Rating of the movie: " + response.data.imdbRating);
-    console.log("---------------Data---------------");
-    console.log("The movie's rating is: " + response.data.Country);
-    console.log("---------------Data---------------");
-    console.log("Language of the movie: " + response.data.Language);
-    console.log("---------------Data---------------");
-    console.log("Plot of the movie: " + response.data.Plot);
-    console.log("---------------Data---------------");
-    console.log("Actors in the movie: " + response.data.Actors);
 
+    
+    if (response.data.Title === "Undefined" ) {
+      console.log(mrNobody);
+      console.log(netflix);
+    } else {
+
+      console.log("---------------Begin Data---------------");
+      console.log("Title of the movie: " + response.data.Title);
+      console.log("---------------movie-this---------------");
+      console.log("Year the movie came out: " + response.data.Year);
+      console.log("---------------movie-this---------------");
+      console.log("IMDB Rating of the movie: " + response.data.imdbRating);
+      console.log("---------------movie-this---------------");
+      console.log("The movie's rating is: " + response.data.Country);
+      console.log("---------------movie-this---------------");
+      console.log("Language of the movie: " + response.data.Language);
+      console.log("---------------movie-this---------------");
+      console.log("Plot of the movie: " + response.data.Plot);
+      console.log("---------------movie-this---------------");
+      console.log("Actors in the movie: " + response.data.Actors);
+      console.log("---------------End Data---------------");
+      
+    }
+
+  })
+  .catch(function(error) {
+    if (error.response) {
+      console.log("---------------Data---------------");
+      console.log(error.response.data);
+      console.log("---------------Status---------------");
+      console.log(error.response.status);
+      console.log("---------------Status---------------");
+      console.log(error.response.headers);
+    } else if (error.request) {
+    
+      console.log(error.request);
+    } else {
+      console.log("Error", error.message);
+    }
+      console.log(error.config);
+  });
+
+};
+
+//---------------------------------
+  
+function concertThis(userInputChoice) {
+  let concertQueryURL = "https://rest.bandsintown.com/artists/" + userInputChoice + "/events?app_id=codingbootcamp";
+  axios.get(concertQueryURL).then(
+    function(response) {
+      for (let i = 0; i < 3; i++) {
+        console.log("-------------------------------searchBandsInTown-------------------------------");
+        console.log("Venue: " + response.data[i].venue.name);
+        console.log("Location: " + response.data[i].venue.city + ", " + response.data[i].venue.country);
+        console.log("Date: " + moment(response.data[i].datetime).format("MM/DD/YYYY"));
+      }
+    })
+    .catch(function(error) {
+      if (error.response) {
+        
+        console.log("---------------Data---------------");
+        console.log(error.response.data);
+        console.log("---------------Status---------------");
+        console.log(error.response.status);
+        console.log("---------------Status---------------");
+        console.log(error.response.headers);
+      } else if (error.request) {
+     
+        console.log(error.request);
+      }
+       else {
+     
+        console.log("Error, no name or locations.", error.message);
+      }
+      console.log(error.config);
+  });
+};
+
+//--------------------
+//concertThis(userInputChoice);
+var anotherArgu = process.argv[3];
+
+function liriBot() {
+  
+  switch (anotherArgu, userInputChoice) {
+  case "movie-this":
+    movieThis(anotherArgu,userInputChoice);
+    break;
+  
+  case "concert-this":
+    concertThis(anotherArgu,userInputChoice);
+    break;
+  
+  case "spotify-this-song":
+    spotifyThisSong(anotherArgu,userInputChoice);
+
+    break;
+  
+  case "do-what-it-says":
+    doWhatItSays();
+    break;
   }
-);
+
+};
+// ---------------------
+liriBot();
+
+ function doWhatItSays() {
+  console.log(readMe);
+}
